b9884131bb3de657ec4353e6bca33ccad26f3620
bernatixer <bernat.torres.bellido@gmail.com>
2019-11-16 17:43:23 +0100

    Adding font file

---
+++ settings.py
@@ -0,0 +1,11 @@
+TILE_SIZE = 32
+
+SIZE = WIDTH, HEIGHT = 480, 480
+
+COLORS = {
+    "black": (0,0,0),
+    "white": (255,255,255),
+    "red": (244,67,54),
+    "text": (76,175,80),
+    "snake": (255,193,7),
+}
